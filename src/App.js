53796e655b05f761a2077c7429acf59e7c9e1ee3
Wellerson Pereira <wellerson_61@hotmail.com>
2020-06-24 00:38:51 -0300

    HTML and half of the CSS is ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="Left_Columm">
+        <h1>Learn to code by</h1>
+        <h1>watching others</h1>
+
+        <p>See how experienced developers solve problems in real-time.
+        Watching scripted tutorials is great, but understanding how
+        developers think is invaluable.</p>
+      </div>
+      <div className="Right_Column">
+        <div className="try-message">
+          <p>Try it free 7 days the $20/mo. thereafter</p>
+        </div>
+
+        <div className="form-box">
+          <form className="form-group">
+            <input type="text" name="firstName" id="firstName" placeholder="First Name" />
+            <input type="text" name="lastName" id="lastName" placeholder="Last Name" />
+            <input type="email" name="email" id="email" placeholder="Email Address" />
+            <input type="password" name="password" id="password" placeholder="Password" />
+
+            <button type="submit">claim your free trial</button>
+            <p>By clicking the button you are agreeing to out <a href="#">Terms and Services</a></p>
+          </form>
+        </div>
+      </div>
+    </body>
   );
 }
 
